4e20ff78e72f1c033524adaa269ba5c762c1488f
Sinaheidari <sinaheidari2000@outlook.com>
2022-06-03 10:42:33 +0000

    q1 created

--- include/q1.h
+++ include/q1.h
@@ -1,4 +1,21 @@
 #ifndef Q1_H
 #define Q1_H
 
-#endif //Q1_H
+#include <cmath>
+
+namespace q1
+{
+    template <typename T, typename Func>
+    double gradient_descent(T initial_value, double step_size, Func func = Func{})
+    {
+        double Dispute{step_size * ((func(initial_value) - func(initial_value - 0.005)) / 0.005)};
+        while (std::abs(Dispute) >= 0.000001)
+        {
+            Dispute = step_size * ((func(initial_value) - func(initial_value - 0.005)) / 0.005);
+            initial_value = initial_value - Dispute;
+        }
+        return initial_value;
+    }
+}
+
+#endif // Q1_H
--- include/q2.h
+++ include/q2.h
@@ -1,4 +1,73 @@
-#ifndef Q2_H
-#define Q2_H
+// #ifndef Q2_H
+// #define Q2_H
 
-#endif //Q2_H
+// #include <iostream>
+// #include <fstream>
+// #include <string>
+// #include <vector>
+// #include <sstream>
+
+// namespace q2
+// {
+//     struct Patient
+//     {
+//         Patient(std::string name,
+//                 size_t age,
+//                 size_t smokes,
+//                 size_t area_q,
+//                 size_t alkhol) : name{name},
+//                                  age{age},
+//                                  smokes{smokes},
+//                                  area_q{area_q},
+//                                  alkhol{alkhol} {}
+//         std::string name;
+//         size_t age;
+//         size_t smokes;
+//         size_t area_q;
+//         size_t alkhol;
+//     };
+
+//     std::vector<Patient> read_file(std::string filename)
+//     {
+//         std::vector<Patient> data;
+//         std::fstream fin;
+//         fin.open(filename, std::ios::in);
+//         std::vector<std::string> row;
+//         std::vector<std::vector<std::string>> alldata;
+//         std::string line, word, temp;
+//         getline(fin, line);
+//         getline(fin, line);
+
+//         while (fin >> temp)
+//         {
+//             row.clear();
+//             getline(fin, line);
+
+//             std::stringstream s(line);
+//             while (getline(s, word, ','))
+//             {
+//                 row.push_back(word);
+//             }
+//             alldata.push_back(row);
+//         }
+//         for (auto i : alldata)
+//         {
+
+//             Patient pa;
+//             if (isspace(i[0].back()))
+//             {
+//                 pa.name = i[0] + i[1];
+//             }
+//             else
+//             {
+//                 pa.name = i[0] + " " + i[1];
+//             }
+//             pa.age = stoi(i[2]);
+//             pa.smokes = stoi(i[3]);
+//             pa.area_q = stoi(i[4]);
+//             data.push_back(pa);
+//         }
+//         return data;
+//     }
+// }
+// #endif // Q2_H
